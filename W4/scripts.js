0fa4ad54160baa80fecbed3a7281713e41cbb652
Isaac Sanchez <idsm_2000@hotmail.com>
2019-10-10 19:07:45 -0500

    Adding changes to landing page

---
+++ W4/scripts.js
@@ -0,0 +1,15 @@
+/**This is the file we are going to work with*/
+
+function requestJSON(){
+
+    var xhttp = new XMLHttpRequest();    
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+      else
+      document.getElementById("demo").innerHTML = "Error retreiving the file";
+    };
+    xhttp.open("GET", "file.txt", true);
+    xhttp.send();
+  }
