3b88af95a9dcd77e13787fee0edf071e7ae3de20
Isaac Sanchez <idsm_2000@hotmail.com>
2019-10-17 00:39:01 -0500

    Section 2 repairs 👌

--- 2-Evidence JavaScript Objects/scriptJS.js
+++ 2-Evidence JavaScript Objects/scriptJS.js
@@ -1,3 +1,8 @@
+var d = new Date();
+        document.getElementById("foot").innerHTML = "CIT 261 - Mobile Application Development 2016 - " + d.getFullYear();
+
+
+
 var person = { 
     firstName: "Isaac", 
     lastName : "Sanchez", 
