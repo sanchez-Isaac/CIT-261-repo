10c0ed34b3fe1d2312a0b5167dbafcd4d4b3e824
Isaac Sanchez <idsm_2000@hotmail.com>
2019-10-24 23:38:59 -0500

    Section 5 Ready ✔✔

--- 5-Evidence Local Storage/scriptLSFdisplay.js
+++ 5-Evidence Local Storage/scriptLSFdisplay.js
@@ -10,6 +10,6 @@ document.getElementById("nameDisp").innerHTML = "<b>The student: </b>" + localSt
 document.getElementById("major").innerHTML = "<b>With the Major: </b>" + localStorage.getItem("majorW") + " <br><br> Added this to the Local Storage and this website: ";
 document.getElementById("contentDisp").innerHTML = localStorage.getItem("contentW");
 document.getElementById("ramdImg").src = localStorage.getItem("imgurlW");
-document.getElementById("codeDisp").innerHTML = localStorage.getItem("codeShW");
+document.getElementById("codeDisp").textContent = localStorage.getItem("codeShW");
 
 
