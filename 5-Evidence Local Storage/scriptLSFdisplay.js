628f737e875d7d3b70d3435cf5af9953390080f4
Isaac Sanchez <idsm_2000@hotmail.com>
2019-10-24 23:32:45 -0500

    Section 5 Fixing error again for the 3rd times

--- 5-Evidence Local Storage/scriptLSFdisplay.js
+++ 5-Evidence Local Storage/scriptLSFdisplay.js
@@ -1,15 +1,9 @@
-
-
-
-
-
-
-
-
-
-
-
-
+  console.log(localStorage.getItem("titleW"));
+  console.log(localStorage.getItem("nameW"));
+  console.log(localStorage.getItem("majorW"));
+  console.log(localStorage.getItem("contentW"));
+  console.log(localStorage.getItem("imgurlW"));
+  console.log(localStorage.getItem("codeShW"));
 
 document.getElementById("titleDisp").innerHTML = localStorage.getItem("titleW");
 document.getElementById("nameDisp").innerHTML = "<b>The student: </b>" + localStorage.getItem("nameW");
