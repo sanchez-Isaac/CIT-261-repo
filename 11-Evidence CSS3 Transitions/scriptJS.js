9bf43653d4db6180c5b2ac5874642d059da90713
Isaac Sanchez <idsm_2000@hotmail.com>
2019-11-22 00:44:02 -0600

    Section 11,12 and 13 ready ✔✔✔

---
+++ 11-Evidence CSS3 Transitions/scriptJS.js
@@ -0,0 +1,10 @@
+var d = new Date();
+        document.getElementById("foot").innerHTML = "CIT 261 - Mobile Application Development 2016 - " + d.getFullYear();
+var objectLS ;
+
+
+
+var can = document.getElementById("canvas1");
+var con = can.getContext("2d");
+con.font = "45px Arial";
+con.strokeText("Isaac Sanchez",50,70);
